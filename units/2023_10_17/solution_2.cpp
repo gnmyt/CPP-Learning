4e697ae42987de0380caa067c8da12ac1cf2fd89
Mathias Wagner <germannewsmaker@gmail.com>
2023-10-17 09:58:14 +0200

    Created the solution_2.cpp

---
+++ units/2023_10_17/solution_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y;
+    cout << "Bitte einen Wert eingeben: ";
+    cin >> x;
+
+    cout << "Bitte noch einen Wert eingeben: ";
+    cin >> y;
+
+    cout << "Der erste eingegebene Wert betraegt " << x << "!" << endl;
+    cout << "Der zweite eingegebene Wert betraegt " << y << "!" << endl;
+}
