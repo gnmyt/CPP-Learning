918fdb13b3fc1d722520d0329f429b25bdd530b5
Mathias Wagner <germannewsmaker@gmail.com>
2023-10-17 09:44:36 +0200

    Created the solution.cpp

---
+++ units/2023_10_17/solution.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    short Var1;
+    int Var2;
+
+    Var1 = 5;
+    Var2 = 100;
+
+    cout << "Var1: " << Var1 << endl;
+    cout << "Var2: " << Var2 << endl;
+}
